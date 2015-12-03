2187ed3f2c379f0db8dc54aa2358ef4af7c766d7
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-03 00:59:10 -0200

    add HTTPContext

--- HTTP/HTTPResponderType/HTTPResponders.swift
+++ HTTP/HTTPResponderType/HTTPResponders.swift
@@ -1,4 +1,4 @@
-// HTTPResponderType.swift
+// HTTPResponders.swift
 //
 // The MIT License (MIT)
 //
@@ -21,6 +21,10 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+
+public protocol HTTPContextResponderType {
+    func respond(context: HTTPContext)
+}
 
 public protocol HTTPResponderType {
     func respond(request: HTTPRequest) throws -> HTTPResponse
