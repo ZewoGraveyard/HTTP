5a3f4181e202ebe811334b3e11bf3886f724cbf6
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-26 18:21:41 -0200

    add Xcode

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
 	name: "HTTP",
 	dependencies: [
         .Package(url: "https://github.com/Zewo/Core.git", majorVersion: 0, minor: 1),
-		.Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 1)
+        .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 1)
 	]
 )
---
+++ Xcode/HTTP/HTTP.h
@@ -0,0 +1,19 @@
+//
+//  HTTP.h
+//  HTTP
+//
+//  Created by Paulo Faria on 12/26/15.
+//  Copyright © 2015 Zewo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HTTP.
+FOUNDATION_EXPORT double HTTPVersionNumber;
+
+//! Project version string for HTTP.
+FOUNDATION_EXPORT const unsigned char HTTPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTTP/PublicHeader.h>
+
+
