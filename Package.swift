dd94f659815a44698f032e58d78f53985a7b08cd
Scott Byrns <contact@scottbyrns.com>
2016-04-10 20:58:10 -0600

    Open-Swift

--- Package.swift
+++ Package.swift
@@ -30,6 +30,6 @@ let package = Package(
         .Package(url: "https://github.com/Zewo/MediaType.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/Zewo/URI.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/SwiftX/S4.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/Open-Swift/S4.git", majorVersion: 0, minor: 3),
     ]
 )
