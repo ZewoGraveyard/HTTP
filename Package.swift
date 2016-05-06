33db6b10b8225de8ee650acba7fbe7df4a92fdaf
Tomohisa Takaoka <tomohisa@jtechs.com>
2016-05-06 19:54:41 +0900

    Update Package

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "HTTP",
     dependencies: [
         .Package(url: "https://github.com/Zewo/MediaType.git", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/tomohisa/URI.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/Zewo/URI.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 6),
     ]
 )
