6f143ef7ac2aa40cb67f7e1a193625dbf18e5386
Paulo Faria <paulo.faria.rl@gmail.com>
2016-09-16 16:23:05 -0300

    update to Swift 3 release

--- Package.swift
+++ Package.swift
@@ -3,8 +3,7 @@ import PackageDescription
 let package = Package(
     name: "HTTP",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/MediaType.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/Zewo/URI.git", majorVersion: 0, minor: 8),
-        .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 9),
+        .Package(url: "https://github.com/Zewo/Core.git", majorVersion: 0, minor: 13),
+        .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 13),
     ]
 )
--- Sources/HTTP/Message/Message.swift
+++ Sources/HTTP/Message/Message.swift
@@ -1,26 +1,9 @@
-// Message.swift
-//
-// The MIT License (MIT)
-//
-// Copyright (c) 2015 Zewo
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+public protocol Message {
+    var version: Version { get set }
+    var headers: Headers { get set }
+    var body: Body { get set }
+    var storage: [String: Any] { get set }
+}
 
 extension Message {
     public var contentType: MediaType? {
@@ -72,7 +55,7 @@ extension Message {
             return connection?.lowercased() == "keep-alive"
         }
 
-        return connection?.lowercased() == "close"
+        return connection?.lowercased() != "close"
     }
 
     public var isUpgrade: Bool {
@@ -94,16 +77,12 @@ extension Message {
     public var storageDescription: String {
         var string = "Storage:\n"
 
-        if storage.count == 0 {
+        if storage.isEmpty {
             string += "-"
         }
 
-        for (offset: index, element: (key: key, value: value)) in storage.enumerated() {
-            string += "\(key): \(value)"
-
-            if index < storage.count - 1 {
-                string += "\n"
-            }
+        for (key, value) in storage {
+            string += "\(key): \(value)\n"
         }
 
         return string
